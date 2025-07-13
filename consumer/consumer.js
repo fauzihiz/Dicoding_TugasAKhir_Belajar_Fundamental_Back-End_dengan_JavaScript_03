cb65a65bc3c40c76f60d1041d16fd993a4a7ecb3
Fauzi Hizbullah <fauzihiz@gmail.com>
2025-07-13 09:28:38 +0700

    edit consumer use process.env

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,22 +1,24 @@
 require('dotenv').config();
 const amqp = require('amqplib');
 const nodemailer = require('nodemailer');
-const axios = require('axios');
+const { Pool } = require('pg');
+const pool = new Pool(); 
+//const axios = require('axios');
 const config = require('./utils/config');
 
 const sendEmail = async (to, content) => {
   const transporter = nodemailer.createTransport({
-    host: config.mail.host,
-    port: Number(config.mail.port),
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
     secure: false,
     auth: {
-      user: config.mail.user,
-      pass: config.mail.password,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
     },
   });
 
   await transporter.sendMail({
-    from: config.mail.user,
+    from: process.env.SMTP_USER,
     to,
     subject: 'Hasil Ekspor Playlist Anda',
     text: 'Berikut adalah data playlist Anda.',
@@ -32,13 +34,31 @@ const sendEmail = async (to, content) => {
 };
 
 const getPlaylistData = async (playlistId) => {
-  try {
-    const response = await axios.get(`${process.env.OPENMUSIC_API_URL}/playlists/${playlistId}/songs`);
-    return response.data.data;
-  } catch (error) {
-    console.error('❌ Gagal mengambil data playlist:', error.message);
-    return null;
-  }
+  const playlistQuery = {
+    text: 'SELECT playlists.id, playlists.name FROM playlists WHERE playlists.id = $1',
+    values: [playlistId],
+  };
+
+  const songsQuery = {
+    text: `SELECT songs.id, songs.title, songs.performer
+           FROM songs
+           JOIN playlistsongs ON songs.id = playlistsongs."songId"
+           WHERE playlistsongs."playlistId" = $1`,
+    values: [playlistId],
+  };
+
+  const playlistResult = await pool.query(playlistQuery);
+  const songsResult = await pool.query(songsQuery);
+
+  if (!playlistResult.rowCount) return null;
+
+  return {
+    playlist: {
+      id: playlistResult.rows[0].id,
+      name: playlistResult.rows[0].name,
+      songs: songsResult.rows,
+    },
+  };
 };
 
 const consume = async () => {
